dc8049ff773392693dcc4637e5d804612d007d28
Midorinekonosakotu <midorineko10801@gmail.com>
2024-06-30 13:03:40 +0900

    コンボ加算の表示
    コンボ加算の表示、GreadとPerfectのimageの表示

--- Assets/Script/Score/ComboManager.cs
+++ Assets/Script/Score/ComboManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
@@ -19,7 +20,7 @@ public class ComboManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (scoreManager.ComboCount10 > 0)
+        if (scoreManager.ComboCount10 > 0)  // Fish10のコンボ数が増えたら
         {
             combo = 0;
             combo = scoreManager.ComboCount10;
@@ -28,7 +29,7 @@ public class ComboManager : MonoBehaviour
         if (scoreManager.ComboCount100 > 0)
         {
             combo = 0;
-            combo = scoreManager.ComboCount100;
+            combo = scoreManager.ComboCount100; // Fish100のコンボ数が増えたら
         }
         comboText.text = combo.ToString();
     }
