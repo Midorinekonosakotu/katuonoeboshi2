cdfbdba1ab644e82775e2605898c40bc7193aed0
komiyasogo <TTC2450SG0020@edu.tech.ac.jp>
2024-07-02 07:39:09 +0900

    シーン２

--- github/Assets/proguramu/goend.cs
+++ github/Assets/proguramu/goend.cs
@@ -1,32 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class goend : MonoBehaviour
 {
-     private float step_time; 
+    GameObject gauge;
+     private float score; 
     // Start is called before the first frame update
     void Start()
    {
-     step_time = 0.0f;
-   }
-    public void Decresegaji()
-    {
-    
+        this.gauge = GameObject.Find("gauge");
     }
-    // Update is called once per frame
+    // Update is called once per frame  
     void Update()
     {
-      step_time += Time.deltaTime; 
-    if (step_time >= 90.0f)  
-     {
-     SceneManager.LoadScene("end", LoadSceneMode.Single); 
-     }
+        if (this.gauge.GetComponent<Image>().fillAmount ==0)
+        {
+            if (score >= 1000)
+            {
+                SceneManager.LoadScene("end_succes", LoadSceneMode.Single);
+            }
+            else if (score <= 1000)
+            {
+                SceneManager.LoadScene("end_failure", LoadSceneMode.Single);
+            }
+        }
     }
-public static int Score()
-    {
-        return Score;
-    }
+}
+public static class GameScoreStatic
+{
+    public static int Score = 0;
+}
 
+public class ScoreChanger
+{
+    public void ScorePlusOne()
+    {
+        GameScoreStatic.Score++;
+    }
 }
