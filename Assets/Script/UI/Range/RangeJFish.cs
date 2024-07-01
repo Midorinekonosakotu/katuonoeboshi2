2fca91ce01efa532aff986e716394447b558ef42
Midorinekonosakotu <midorineko10801@gmail.com>
2024-07-01 22:56:47 +0900

    距離ゲージの実装(途中)

---
+++ Assets/Script/UI/Range/RangeJFish.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeJFish : MonoBehaviour
+{
+    public RectTransform JFish;
+    private float move = 0.002f;
+    [SerializeField] int counter = 2600;
+    bool countStop = false;
+    public bool WaveFlag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JFish.position += new Vector3(-move, 0, 0);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            WaveFlag = true;
+        }
+        
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            WaveFlag = false;
+        }
+
+
+        if(WaveFlag == true)
+        {
+            if (countStop == false)
+            {
+                move = 0.004f;
+                counter -= 2;
+            }
+        }
+        else
+        {
+            if (countStop == false)
+            {
+                move = 0.002f;
+                counter--;
+            }
+        }
+
+        if (counter < -1)
+        {
+            countStop = true;
+            move = 0f;
+        }
+    }
+}
